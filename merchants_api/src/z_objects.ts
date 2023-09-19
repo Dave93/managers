e7d5595a671ff6624ff2d59b2b9e579b23a923d4
Davr <davr@Davrs-NewiMac.local>
2023-09-19 17:00:19 +0500

    dasdsa

---
+++ merchants_api/src/z_objects.ts
@@ -0,0 +1,41 @@
+import { z } from "zod";
+
+export const paymeReportInputSchema = z.object({
+  date: z.string(),
+  serviceIds: z.array(z.string()),
+  businessId: z.string(),
+  time: z.string().nullish(),
+});
+
+export const clickReportInputSchema = z.object({
+  date: z.string(),
+  serviceIds: z.array(z.string()),
+  time: z.string().nullish(),
+});
+
+export const yandexReportInputSchema = z.object({
+  date: z.string(),
+  serviceIds: z.array(z.string()),
+  organization_code: z.string(),
+  time: z.string().nullish(),
+});
+
+export const iikoCachierReportInputSchema = z.object({
+  date: z.string(),
+  groupId: z.string(),
+});
+
+export const expressReportInputSchema = z.object({
+  date: z.string(),
+  terminal_id: z.string(),
+  organization_code: z.string(),
+  time: z.string().nullish(),
+});
+
+export const arrytReportInputSchema = z.object({
+  date: z.string(),
+  terminal_id: z.string(),
+  time: z.string().nullish(),
+});
+
+export const davr = "test";
