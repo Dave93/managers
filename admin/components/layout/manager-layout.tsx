8dcdb365e3915d1f7965984666a8e18a8c6bc8cf
i7671757 <ilhombek2110@gmail.com>
2024-05-13 16:11:10 +0500

    sdfgh

--- admin/components/layout/manager-layout.tsx
+++ admin/components/layout/manager-layout.tsx
@@ -8,7 +8,7 @@ export default function ManagerLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="container">
+    <div className="md:container">
       {children}
       <div className="fixed bottom-0 left-0 z-50 w-full h-16 bg-white border-t border-gray-200 dark:bg-gray-700 dark:border-gray-600">
         <div className="grid h-full max-w-lg grid-cols-5 mx-auto font-medium">
