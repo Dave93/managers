58d7d614f399c6e5b215f16c9606ee69a3459d81
Ilhombek <ilhombek2110@gmail.com>
2024-08-20 11:00:12 +0500

    bbbb

--- cron/src/index.ts
+++ cron/src/index.ts
@@ -11,7 +11,7 @@ cron.schedule("0 6 * * *", async () => {
   await terminalService.getTerminalsFromIiko();
 });
 
-cron.schedule("*/5 * * * *", async () => {
+cron.schedule("30 5 * * *", async () => {
   console.log("Running a task every minute");
   await iikoDictionariesService.getIikoDictionariesFromIiko();
 });
