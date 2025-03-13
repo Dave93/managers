703388c1dac173314e8673f9da3408d3135f0f90
Davron Yuldashev <davr@Davrons-Mac-Pro.local>
2025-03-13 20:55:03 +0500

    fixed conflicts

--- admin/public/product_groups/DndContextClient.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-import React from 'react';
-import { DndContext, type DndContextProps } from '@dnd-kit/core';
-
-// This component is only used on the client side
-export default function DndContextClient(props: DndContextProps) {
-    return <DndContext {...props} />;
-} 
--- admin/public/product_groups/DragOverlayClient.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-import React from 'react';
-import { DragOverlay, type DragOverlayProps } from '@dnd-kit/core';
-
-// This component is only used on the client side
-export default function DragOverlayClient(props: DragOverlayProps) {
-    return <DragOverlay {...props} />;
-} 
